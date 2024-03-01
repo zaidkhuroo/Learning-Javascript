20fa096c0a7319628f6fc7e45a472db441c9bee9
Zaid Bin Farooq <108170101+zaidkhuroo@users.noreply.github.com>
2024-03-01 20:47:43 +0530

    basics of type conversion

---
+++ 01_Basics/typeConversion.js
@@ -0,0 +1,50 @@
+// Assign the string 'zaid' to variable 'a'
+let a = 'zaid'
+
+// Display the data type of variable 'a'
+console.log(typeof(a))
+
+// Convert the string 'zaid' to a number and assign it to variable 'b'
+let b = Number(a)
+
+// Display the value of variable 'b'
+console.log(b)
+
+// Display the data type of variable 'b'
+console.log(typeof(b))
+
+// --------------------------------
+console.log('--------------------------------');
+
+// Assign the boolean value 'true' to variable 'z'
+let z = true
+
+// Display the data type of variable 'z'
+console.log(typeof(z))
+
+// Convert the boolean value 'true' to a string and assign it to variable 'y'
+let y = String(z)
+
+// Display the value of variable 'y'
+console.log(y)
+
+// Display the data type of variable 'y'
+console.log(typeof(y))
+
+// --------------------------------
+console.log('--------------------------------');
+
+// Assign the string 'zaid' to variable 'd'
+let d = 'zaid'
+
+// Display the data type of variable 'd'
+console.log(typeof(d))
+
+// Convert the string 'zaid' to a boolean and assign it to variable 'e'
+let e = Boolean(d)
+
+// Display the value of variable 'e'
+console.log(e)
+
+// Display the data type of variable 'e'
+console.log(typeof(e))
