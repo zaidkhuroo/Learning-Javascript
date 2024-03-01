1ca017d82440b043a57748666531d452267942bb
Zaid Bin Farooq <108170101+zaidkhuroo@users.noreply.github.com>
2024-03-01 20:58:23 +0530

    Convert string to number, boolean to string, and handle edge cases.

--- 01_Basics/typeConversion.js
+++ 01_Basics/typeConversion.js
@@ -1,50 +1,62 @@
 // Assign the string 'zaid' to variable 'a'
-let a = 'zaid'
+let a = 'zaid' // string
 
 // Display the data type of variable 'a'
-console.log(typeof(a))
+console.log(typeof(a)) // string
 
 // Convert the string 'zaid' to a number and assign it to variable 'b'
-let b = Number(a)
+let b = Number(a) // number
 
 // Display the value of variable 'b'
-console.log(b)
+console.log(b) // NaN
 
 // Display the data type of variable 'b'
-console.log(typeof(b))
+console.log(typeof(b)) // number
 
 // --------------------------------
-console.log('--------------------------------');
 
 // Assign the boolean value 'true' to variable 'z'
-let z = true
+let z = true // boolean
 
 // Display the data type of variable 'z'
-console.log(typeof(z))
+console.log(typeof(z)) // boolean
 
 // Convert the boolean value 'true' to a string and assign it to variable 'y'
-let y = String(z)
+let y = String(z) // string
 
 // Display the value of variable 'y'
-console.log(y)
+console.log(y) // "true"
 
 // Display the data type of variable 'y'
-console.log(typeof(y))
+console.log(typeof(y)) // string
 
 // --------------------------------
-console.log('--------------------------------');
 
 // Assign the string 'zaid' to variable 'd'
-let d = 'zaid'
+let d = 'zaid' // string
 
 // Display the data type of variable 'd'
-console.log(typeof(d))
+console.log(typeof(d)) // string
 
 // Convert the string 'zaid' to a boolean and assign it to variable 'e'
-let e = Boolean(d)
+let e = Boolean(d) // boolean
 
 // Display the value of variable 'e'
-console.log(e)
+console.log(e) // true
 
 // Display the data type of variable 'e'
-console.log(typeof(e))
+console.log(typeof(e)) // boolean
+
+// --------------------------------
+
+let n = "33a" // string
+let o = Number(n) // number
+console.log(o) // 33
+console.log(typeof(o)) // number
+
+// --------------------------------
+
+let bol = 0 // number
+let top = Boolean(bol) // boolean
+console.log(top) // false
+console.log(typeof(top)) // boolean
