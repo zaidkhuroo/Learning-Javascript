42480eff8b408efdbd8f276e48c4e6d520972113
Zaid Bin Farooq <108170101+zaidkhuroo@users.noreply.github.com>
2024-03-02 23:26:18 +0530

    Convert boolean and string to number.

--- 01_Basics/typeConversion.js
+++ 01_Basics/typeConversion.js
@@ -13,6 +13,8 @@ console.log(b) // NaN
 // Display the data type of variable 'b'
 console.log(typeof(b)) // number
 
+console.log("=====================================");
+
 // --------------------------------
 
 // Assign the boolean value 'true' to variable 'z'
@@ -32,6 +34,8 @@ console.log(typeof(y)) // string
 
 // --------------------------------
 
+console.log("=====================================");
+
 // Assign the string 'zaid' to variable 'd'
 let d = 'zaid' // string
 
@@ -49,6 +53,8 @@ console.log(typeof(e)) // boolean
 
 // --------------------------------
 
+console.log("=====================================");
+
 let n = "33a" // string
 let o = Number(n) // number
 console.log(o) // 33
@@ -56,7 +62,32 @@ console.log(typeof(o)) // number
 
 // --------------------------------
 
+console.log("=====================================");
+
 let bol = 0 // number
 let top = Boolean(bol) // boolean
 console.log(top) // false
 console.log(typeof(top)) // boolean
+
+
+console.log("=====================================");
+// The 'console.log' function is used to print the output of expressions to the console.
+
+// This line prints the boolean value 'true' converted to a number (1)
+console.log(+true); // 1
+
+// This line tries to convert an empty string to a number, resulting in 0
+console.log(+""); // 0
+
+// This line tries to convert the string 'zaid' to a number, which results in NaN (Not-a-Number)
+console.log(+'zaid'); // NaN
+
+// This line performs numeric addition of two numbers: 2 + 2
+console.log(2+2); // 4
+
+// This line concatenates the string '2' with the number 2 and then with the string '3',
+// resulting in '223'
+console.log('2'+ 2+3); // '223'
+
+// This line performs numeric addition of 9 and 11 and then concatenates the result with the string '32'
+console.log(9+11+'32'); // '2032'
