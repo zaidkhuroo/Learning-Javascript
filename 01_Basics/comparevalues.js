76c8fe675b2b60e13ec3d51e092c7953a43948d0
Zaid Bin Farooq <108170101+zaidkhuroo@users.noreply.github.com>
2024-03-03 00:11:53 +0530

    comparing values of similar and dissimilar datatypes

---
+++ 01_Basics/comparevalues.js
@@ -0,0 +1,9 @@
+console.log(22 > 3);
+console.log('2' > 3);
+console.log(null == 0);//these type of convertions should be avoided
+console.log(null >= 0);//these type of convertions should be avoided
+console.log(null <= 0);//these type of convertions should be avoided
+console.log(null > 0);//these type of convertions should be avoided
+console.log(undefined == 0);//these type of convertions should be avoided
+console.log(undefined <= 0);//these type of convertions should be avoided
+console.log('2' === 2); //strict check
