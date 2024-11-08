0323700c21f5809a98acf71da5174427375a2c4c
Zaid Bin Farooq <khuroozaid@gmail.com>
2024-11-08 15:28:55 +0000

    Exercise

---
+++ 01_Basics/exercise/guessGame.js
@@ -0,0 +1,6 @@
+let a = 10;
+let n = prompt("Enter a number");
+while (n != a) {
+    n= prompt("try again");
+}
+alert("right");
---
+++ 01_Basics/exercise/usernameGenerate.js
@@ -0,0 +1,11 @@
+let a = prompt("Enter Input");
+let b = "@";
+let res = b.concat(a);
+let l = res.length;
+let final = res.concat(l);
+console.log(final);
+
+// or
+/* 
+let full = "@" + a + full.length;
+*/
