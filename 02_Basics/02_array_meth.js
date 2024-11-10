30782110700ee567c08e6b065451af4fdc5c71a5
Zaid Bin Farooq <khuroozaid@gmail.com>
2024-11-10 16:59:07 +0000

    various array methods

--- 02_Basics/02_array_meth.js
+++ 02_Basics/02_array_meth.js
@@ -11,12 +11,43 @@ console.log(my_array);
 console.log("-------------------------------------\n");
 let new_array = [1, 2, 3, [4, 5, 6, 7], 8, 9, [10, 11, [12, 13,], 14, 15], 16]; // nested array example
 console.log(new_array.flat(6));// flattening the array by removing nested array and keep the elements in one dimension
-let arr1=200
-let arr2=300
-let arr3=400
+let arr1 = 200
+let arr2 = 300
+let arr3 = 400
 console.log("-------------------------------------\n");
-console.log(Array.of(arr1,arr2,arr3)); //  creating a new array with given elements
+console.log(Array.of(arr1, arr2, arr3)); //  creating a new array with given elements
 console.log("-------------------------------------\n");
 console.log(Array.isArray('Zaid')) //  false because string is not an array
 console.log("-------------------------------------\n");
 console.log(Array.from('Zaid'))    // ['Z', 'a', 'i', 'd']   converting a string into an array using from method.
+
+// filter method
+let myarr = [1, 2, 3, 4, 5];
+let evenarray = myarr.filter((val) => {
+    return val % 2 === 0;
+
+});
+console.log(evenarray);
+
+// reduce method
+let add = myarr.reduce((prev, curr) => {
+    return prev + curr;
+
+});
+
+console.log(add);
+
+// E.g 2
+let greatestnum = myarr.reduce((prev, curr) => {
+    if (prev > curr) {
+        return prev;
+    }
+    else {
+        return curr;
+    }
+
+    // other way
+    // return prev>curr?prev:curr;
+});
+
+console.log(greatestnum);
--- 02_Basics/08_forEachLoop.js
+++ 02_Basics/08_forEachLoop.js
@@ -8,3 +8,23 @@ myarr.forEach((val, idx, myarr) => {
     console.log(val, idx, myarr);  
     
 });
+
+// practice
+let mynumbers = [1, 2, 3, 4];
+let square=0;
+mynumbers.forEach((sqr) => { 
+
+square =sqr*sqr;
+console.log(square); 
+
+});
+
+
+// other method
+
+let myar = [1, 2, 3, 4];
+myar.forEach((sqr) => { 
+
+console.log(sqr*sqr); 
+
+});
